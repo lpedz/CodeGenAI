e84eecdfac4de0bb2a0ac6aeaeac3d513631d4df
Barkabu <barkabu322@gmail.com>
2024-07-24 23:50:08 +0530

    Changed API to use google's Gemini and changed template

--- llama/app.py
+++ llama/app.py
@@ -1,29 +1,19 @@
-import streamlit as st  # Streamlit is a library used for creating web applications with simple Python scripts.
-from langchain.prompts import PromptTemplate  # Used to give prompts to the LLM
-from langchain.llms import CTransformers
+import google.generativeai as genai
+import os
+import streamlit as st
 
-## Function To get response from LLaMA 2 model
-def getLLamaResponse(input_text, programming_language):
-    ### LLaMA2 model
-    llm = CTransformers(
-        model='TheBloke/Llama-2-7B-Chat-GGML',
-        model_type='llama',
-        config={'max_new_tokens': 256, 'temperature': 0.5}
-    )  # CTransformers is used to call the LLaMA 2 model.
 
-    ## Prompt Template: the prompt that is given to the LLaMA-2 model, which is then processed to generate the output
-    template = """
-        Write a functional code snippet in {programming_language} for the following task:
-        {input_text}
-    """
-    prompt = PromptTemplate(input_variables=["programming_language", "input_text"], template=template)
+apikey="AIzaSyCqO6PIgY80nLSW1iJyI4BvCe2PflVsloY"
 
-    ## Generate the response from the LLaMA 2 model
-    response = llm(prompt.format(programming_language=programming_language, input_text=input_text))
-    print(response)
-    return response
+genai.configure(api_key=apikey)
 
-# Setting Streamlit UI
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+
+def generate(text,style):
+    response = model.generate_content("Genrate a story of the context"+text+"in the style of"+style)
+    return response.text
+
 st.set_page_config(
     page_title="Generate Code Snippets",
     page_icon='🤖',
@@ -31,16 +21,16 @@ st.set_page_config(
     initial_sidebar_state='collapsed'
 )
 
-st.header("Generate Code Snippets")
+st.header("Generate Story")
 
-input_text = st.text_area("Describe the task for which you need a code snippet")
+input_text = st.text_area("Describe the context you need story for")
 
 ## Creating a dropdown to select the programming language
-programming_language = st.selectbox('Select the Programming Language', ('Python', 'JavaScript', 'Java', 'C++'), index=0)
+writingStyle = st.selectbox('Select the writing style',( '15th century', '5yr olds', 'Modern', 'simple'), index=0)
 
 # Button to submit the request
 submit = st.button("Generate")
 
 ## Display the final response
 if submit:
-    st.code(getLLamaResponse(input_text, programming_language), language=programming_language.lower())
+    st.code(generate(input_text,writingStyle))
